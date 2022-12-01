369d9c9bea9ca81f425feb0de3f2db1c3b8fcb46
lewy054 <lewy054@gmail.com>
2022-12-02 00:25:21 +0100

    Add Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,55 @@
+﻿var file = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+PartOne(file);
+PartTwo(file);
+
+void PartOne(string filePath)
+{
+    var mostCalories = 0;
+    var currentCalories = 0;
+    foreach (var line in File.ReadLines(filePath))
+    {
+        if (!string.IsNullOrEmpty(line))
+        {
+            currentCalories += int.Parse(line);
+        }
+        else
+        {
+            if (currentCalories > mostCalories)
+            {
+                mostCalories = currentCalories;
+            }
+
+            currentCalories = 0;
+        }
+    }
+
+    Console.WriteLine(mostCalories);
+}
+
+void PartTwo(string filePath)
+{
+    var mostCalories = new List<int> { 0, 0, 0 };
+    var currentCalories = 0;
+    foreach (var line in File.ReadLines(filePath))
+    {
+        if (!string.IsNullOrEmpty(line))
+        {
+            currentCalories += int.Parse(line);
+        }
+        else
+        {
+            foreach (var mostCalorie in mostCalories)
+            {
+                if (currentCalories > mostCalorie)
+                {
+                    var minValue = mostCalories.Min();
+                    mostCalories[mostCalories.IndexOf(minValue)] = currentCalories;
+                    break;
+                }
+            }
+
+            currentCalories = 0;
+        }
+    }
+    Console.WriteLine(mostCalories.Sum());
+}
